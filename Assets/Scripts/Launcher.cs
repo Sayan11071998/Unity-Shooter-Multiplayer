166fc816e75df3bf3b6fc906fa3c514333811eac
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-08-31 19:32:36 +0530

    Leaving a Room

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -98,4 +98,18 @@ public class Launcher : MonoBehaviourPunCallbacks
         CloseMenus();
         menuButtons.SetActive(true);
     }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        CloseMenus();
+        loadingText.text = "Leaving Room...";
+        loadingScreen.SetActive(true);
+    }
+
+    public override void OnLeftRoom()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
 }
