9e14a60f998ce526f438e6b1227fa9b018f1416c
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2025-08-31 18:42:54 +0530

    Connecting To Server

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+    public GameObject menuButtons;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        CloseMenus();
+
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting To Network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        loadingText.text = "Joining Lobby...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+}
